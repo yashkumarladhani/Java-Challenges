4a8d64536f0cd74689f2c1cdf56ff5e875e8c27b
Yash Kumar Ladhani <115504025+yashkumarladhani@users.noreply.github.com>
2024-02-24 21:07:45 +0500

    Challenge: 01
    Using Single Print Statement print the Right half Pyramid

---
+++ 1righthalfpyramid.java
@@ -0,0 +1,9 @@
+class righthalfpyramid {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}
